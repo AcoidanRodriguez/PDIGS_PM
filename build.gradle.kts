391bfab1e691f9c699f521a4b53bc5b4ae5f1fbe
Acoidán Rodríguez Peña <114836241+AcoidanRodriguez@users.noreply.github.com>
2024-05-10 09:57:26 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,13 @@ plugins {
     id("com.android.application") version "8.1.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
     id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
+    id("org.sonarqube") version "4.4.1.3373"
+}
+
+sonar {
+  properties {
+    property("sonar.projectKey", "AcoidanRodriguez_PDIGS_PM")
+    property("sonar.organization", "acoidanrodriguez-1")
+    property("sonar.host.url", "https://sonarcloud.io")
+  }
 }
